af646a5e524836461273c518c31da401d9e5c783
xjchong <xavier.chong.j@gmail.com>
2019-06-19 17:01:25 -0400

    chore: show activities from pages

--- app/src/main/java/com/bestCatHustlers/sukodublitz/GameActivity.java
+++ app/src/main/java/com/bestCatHustlers/sukodublitz/GameActivity.java
@@ -1,7 +1,9 @@
 package com.bestCatHustlers.sukodublitz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    public void openResultsActivity(View view) {
+        Intent intent = new Intent(this, ResultsActivity.class);
+
+        startActivity(intent);
     }
 }
