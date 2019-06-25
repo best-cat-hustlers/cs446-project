c0acd08049203d3aa60a9e8135f8150b7a95ed2f
JXZhang <zjiaxi@edu.uwaterloo.ca>
2019-06-25 17:24:30 -0400

    temp commit

--- app/src/main/java/com/bestCatHustlers/sukodublitz/GameSetupActivity.java
+++ app/src/main/java/com/bestCatHustlers/sukodublitz/GameSetupActivity.java
@@ -1,9 +1,10 @@
 package com.bestCatHustlers.sukodublitz;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import com.bestCatHustlers.sukodublitz.game.GameActivity;
 
@@ -17,5 +18,15 @@ public class GameSetupActivity extends AppCompatActivity {
     public void clickGame(View view) {
         Intent intent = new Intent(this, GameActivity.class);
         this.startActivity(intent);
+    }
+    public void clickDifficulty(View view){
+        Button diffButton = (Button) view;
+        changeHighlightDifficulty(diffButton.getId());
+
+    }
+    private void changeHighlightDifficulty(int id) {
+        String [] allDifficulties = {"diff1", "diff2", "diff3", "diff4", "diff5"};
+
+
     }
 }
