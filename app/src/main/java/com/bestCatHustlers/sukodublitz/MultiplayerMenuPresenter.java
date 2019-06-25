e552f67f60156d0a2317a0b884bc1c03113ba310
Steven Yang <stevenwaterloo2015@gmail.com>
2019-06-25 15:23:17 -0400

    fix typo

--- app/src/main/java/com/bestCatHustlers/sukodublitz/MultiplayerMenuPresenter.java
+++ app/src/main/java/com/bestCatHustlers/sukodublitz/MultiplayerMenuPresenter.java
@@ -7,6 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class MultiplayerMenuPresentor {
+public class MultiplayerMenuPresenter {
 
 }
