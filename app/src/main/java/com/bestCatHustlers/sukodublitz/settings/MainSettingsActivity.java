b1366319c06d65ba74217b9dd2853de6afc314e7
Jiaxi Zhang <ninasax@outlook.com>
2019-07-20 16:48:35 -0400

    Hide keyboard and show toast message when press update user name

--- app/src/main/java/com/bestCatHustlers/sukodublitz/settings/MainSettingsActivity.java
+++ app/src/main/java/com/bestCatHustlers/sukodublitz/settings/MainSettingsActivity.java
@@ -1,11 +1,15 @@
 package com.bestCatHustlers.sukodublitz.settings;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.bestCatHustlers.sukodublitz.R;
 
@@ -63,6 +67,17 @@ public class MainSettingsActivity extends AppCompatActivity implements MainSetti
         EditText editText = findViewById(R.id.userName);
         presenter.setUserName(editText.getText().toString());
         editText.setCursorVisible(false);
+        // Hides keyboard
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        assert inputMethodManager != null;
+        inputMethodManager.hideSoftInputFromWindow(editText.getWindowToken(),0);
+        // Show toast message
+        Context context = getApplicationContext();
+        CharSequence text = "User name is updated";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
     }
     public void onClickEdit(View view) {
         ((EditText)view).setCursorVisible(true);
