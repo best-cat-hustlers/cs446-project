794dab5202f33fa4b1ca0081c663581874ebd863
xjchong <xavier.chong.j@gmail.com>
2019-07-23 13:37:54 -0400

    ref: remove unused imports

--- app/src/main/java/com/bestCatHustlers/sukodublitz/lobby/LobbyActivity.java
+++ app/src/main/java/com/bestCatHustlers/sukodublitz/lobby/LobbyActivity.java
@@ -2,7 +2,6 @@ package com.bestCatHustlers.sukodublitz.lobby;
 
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
@@ -10,20 +9,12 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
-import android.os.Parcelable;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.bestCatHustlers.sukodublitz.ExtrasKeys;
-import com.bestCatHustlers.sukodublitz.GameAI;
-import com.bestCatHustlers.sukodublitz.GameSetupActivity;
 import com.bestCatHustlers.sukodublitz.R;
 import com.bestCatHustlers.sukodublitz.bluetooth.BluetoothConstants;
 import com.bestCatHustlers.sukodublitz.bluetooth.BluetoothService;
